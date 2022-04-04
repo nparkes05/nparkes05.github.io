dee3e58ca9a9683c465bd4f351bcbdebbffcbb16
nparkes05 <nparkes@uncc.edu>
2022-04-04 01:32:02 -0400

    midterm

---
+++ itis3135/hobby/hobby.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $('#homeHeading').click(function(){
+        $('#home').slideDown("slow");
+    });
+});
--- itis3135/scripts/slideshow.js
+++ itis3135/scripts/slideshow.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
     const jpegImage = [
-        "../images/angryDog.jpeg",
-        "../images/cat.jpeg",
-        "../images/maddyBlanket.jpeg",
-        "../images/riley.jpeg",
-        "../images/woodyAndMaddy.jpeg",
-        "../images/youngRiley.jpeg"
+        "images/angryDog.jpeg",
+        "images/cat.jpeg",
+        "images/maddyBlanket.jpeg",
+        "images/riley.jpeg",
+        "images/woodyAndMaddy.jpeg",
+        "images/youngRiley.jpeg"
     ]
     let count = 0;
     var image = new Image();
